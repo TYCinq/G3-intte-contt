f9b4156b119a42999788c92509a3e45cb319beef
TYCinq <92715642+TYCinq@users.noreply.github.com>
2023-12-04 10:08:21 +0100

    Add files via upload

---
+++ test_unitaire.js
@@ -0,0 +1,38 @@
+// npm install mocha
+// npm install assert
+// npm install mock-local-storage
+
+
+// pannier site
+function addToCart (productName, price) {
+    var cart = JSON.parse(localStorage.getItem('cart')) || [];
+    var newItem = { productName: productName, price: price};
+    cart.push(newItem);
+    localStorage.setItem('cart', JSON.stringify(cart));
+    return cart;
+}
+
+module.exports = addToCart
+
+
+//test 
+
+const assert = require ('assert');
+const mock = require ('mock-local-storage');
+const addToCart = require ('./cart');
+
+before(() => {
+    global.localStorage = mock.localStorage();
+});
+
+describe('addItemToCart', function() {
+    it('ajouter un objet au pannier', function() {
+        addToCart('Product 1', 10.99);
+        const cart = JSON.parse(localStorage.getItem('cart'));
+
+        assert.strictEqual(cart.length, 1);
+        assert.strictEqual(cart[0].productName, 'Product 1');
+        assert.strictEqual(cart[0].price, 10.99);
+    });
+
+});
