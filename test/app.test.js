740309fd88f671a4d9e223841b98e265bbc3e3de
TIETI <thierry.barbier4@epsi!,fr>
2024-01-22 21:10:37 +0100

    valid

--- test/app.test.js
+++ test/app.test.js
@@ -1,28 +1,22 @@
 const mocha = require('mocha');
-const expect = require('assert').strict;  // Utilisation de l'assertion native de Node.js
+const expect = require('assert').strict; 
 
-// Fonction addToCart
 const addToCart = {
   calculerLePrixTotal: function (panier) {
-    // Logique pour calculer le prix total
     panier.prixTotal = panier.prix * panier.quantité;
   }
 };
 
-// Suite de tests
 describe('addToCart', () => {
   it('devrait vérifier le contenu de la variable', () => {
-    // Arrange
     const panier = {
       nomProduit: 'clavier XXX',
       prix: 75.00,
       quantité: 1,
     };
 
-    // Act
     addToCart.calculerLePrixTotal(panier);
 
-    // Assert
     if (panier.prixTotal === 75.00) {
       console.log('Test réussi !');
     } else {
